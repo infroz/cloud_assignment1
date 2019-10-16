fb0a23da404386b2a092c00942f3ac49ddb32bbc
Infroz <infroz98@gmail.com>
2019-10-16 20:14:13 +0200

    country get method added

--- handler.go
+++ handler.go
@@ -2,6 +2,7 @@ package assignment1
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -23,5 +24,26 @@ func HandlerCountry(w http.ResponseWriter, r *http.Request) {
 	country := parts[2]
 
 	fmt.Fprintf(w, country)
+
+	countryAPI := "https://restcountries.eu/rest/v2/name/"
+	fmt.Println("Running GET: " + countryAPI + country)
+	req, err := http.NewRequest(http.MethodGet, countryAPI+country, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(body))
 
 }
