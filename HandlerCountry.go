4cc5928b4d6ccc004ccaa6f55377ec0510ed2ec8
Infroz <infroz98@gmail.com>
2019-10-17 19:52:10 +0200

    Commented my code further

--- HandlerCountry.go
+++ HandlerCountry.go
@@ -10,36 +10,43 @@ import (
 	"strings"
 )
 
-// HandlerCountry - - -s
+// HandlerCountry - This function handles country requests
 func HandlerCountry(w http.ResponseWriter, r *http.Request) {
+	// Splits url into readable parts
+	// url/[1]Country/[2]{CountryCode}
 	parts := strings.Split(r.URL.Path, "/")
 	if len(parts) != 3 {
 		status := http.StatusBadRequest
 		http.Error(w, "Expecting format .../countryName", status)
 		return
 	}
-	country := parts[2]
+	country := parts[2] // Sets variable country to country code from GET
 
-	fmt.Fprintf(w, country)
-
+	// API link for alpha2Code
 	countryAPI := "https://restcountries.eu/rest/v2/alpha/"
+
+	// Print to Console for visualization of what is happening
 	fmt.Println("Running GET: " + countryAPI + country)
+
+	// Preparing GET statement
 	req, err := http.NewRequest(http.MethodGet, countryAPI+country+"?fields=name;alpha2Code;flag", nil)
 	if err != nil {
-		panic(err)
+		panic(err) // Error handling
 	}
 
+	// Executing GET statement
 	client := http.DefaultClient
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		panic(err) // Error handling
 	}
 
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body) // Sets JSON into body
 	if err != nil {
 		panic(err)
 	}
+	// Print out values
 	fmt.Fprintf(w, string(body)+"\n\nDecoded Data:\n")
 
 	dec := json.NewDecoder(strings.NewReader(string(body)))
