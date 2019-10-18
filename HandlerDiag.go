fa2731c965c9add87d8f781b779686c47d07aa35
Infroz <infroz98@gmail.com>
2019-10-18 07:52:05 +0200

    Uptime working, returns full JSON - GBIF and REST status not implemented

--- HandlerDiag.go
+++ HandlerDiag.go
@@ -1,6 +1,7 @@
 package assignment1
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"time"
@@ -11,14 +12,29 @@ var StartTime time.Time
 
 // Uptime - Returns times in seconds since service start
 func Uptime() time.Duration {
-	return time.Since(StartTime)
+	return time.Since(StartTime) / 1000000000
 }
 
 // HandlerDiag - Handles Get requests for /diag/
 func HandlerDiag(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		log.Println(Uptime())
+		var d Diag
+		d.Gbif = 200          // FakeOK
+		d.Restcountries = 200 // FakeOK
+		d.Version = "v1"
+		d.Uptime = int(Uptime())
+
+		// Encode new structure to JSON format
+		enc, err := json.Marshal(d)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Gives JSON response for requests
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(enc)
 	default:
 		// For any other method than GET - undefined
 		http.Error(w, "not implemented yet", http.StatusNotImplemented)
