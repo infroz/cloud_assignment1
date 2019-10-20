e7d9922cf1e4addaad0f23e6314278aeeb174018
Infroz <infroz98@gmail.com>
2019-10-20 18:41:59 +0200

    Get request added

--- HandlerSpecies.go
+++ HandlerSpecies.go
@@ -14,8 +14,16 @@ func HandlerSpecies(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Expecting format .../conservation/v1/species/", status)
 		return
 	}
+	// Get limit on results
+	Limit := r.URL.Query().Get("limit")
+	if Limit == "" {
+		Limit = "30" // Default limit
+	}
+	// API URL
+	getAPI := "http://api.gbif.org/v1/occurrence/search?country=" + parts[4] + "&limit=" + Limit
 
-	//speciesAPI := ""
+	Client := http.DefaultClient
+	resp := GetRequest(Client, getAPI)
 
 	fmt.Fprintln(w, parts)
 }
